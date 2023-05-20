5b060f648f0532b6e7be24d1d627345321e8670d
uzairansari11 <uzairans532@gmail.com>
2023-05-20 12:47:41 +0530

    chatpage and  home page setup

---
+++ frontend_chatapp/src/Utils/fetchingchat.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+export const chatDataFromAPI = async (url) => {
+	try {
+		const { data } = await axios.get(url);
+		return data;
+	} catch (error) {
+		console.log(error.message);
+	}
+};
