5e9ae86ee634be130d2de77949115b4dd34935b1
uzair ansari <uzairans532@gmail.com>
2024-04-23 14:23:03 +0530

    signup import properly

--- frontend_chatapp/src/Components/Authentication/AuthenticationTabs.jsx
+++ frontend_chatapp/src/Components/Authentication/AuthenticationTabs.jsx
@@ -8,8 +8,7 @@ import {
   SlideFade,
 } from "@chakra-ui/react";
 import Login from "./Login";
-import SignUp from "./SignUp";
-
+import SignUp from "./SignUp"
 const AuthenticationTabs = () => {
   const [tabIndex, setTabIndex] = useState(0);
   const tabNames = ["Login", "Sign Up"];
